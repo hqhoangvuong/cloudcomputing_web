429a6bf1c7f8c237c608feff835682cf7a016988
Vuong Quoc Hoang Huynh <vuongqhhuynh@kms-technology.com>
2020-07-08 07:56:59 +0700

    Final of Final

--- public/js/action.js
+++ public/js/action.js
@@ -19,34 +19,13 @@ function checkTime(i) {
 $(document).ready(function(){
     //Lấy độ ẩm, nhiệt độ, thời gian gần nhất.
     getCurrentWeather();
-
+    setBackground();
 })
 
 
 function getCurrentWeather(){
-        //Ajax get current
-         $.ajax({
-             crossDomain: true,
-             method: "GET",
-             contentType: "application/json; charset=UTF-8",
-             cache: true,  
-             url: "https://weather-api-cors.herokuapp.com/iot", 
-             dataType: 'jsonp',
-             headers: {
-                 "accept": "application/json",
-                 'Access-Control-Allow-Origin': '*',
-                 'Access-Control-Allow-Credentials': 'true',
-                 "Access-Control-Allow-Methods": "GET",
-                 'Access-Control-Allow-Headers': '*',
-                 'Access-Control-Request-Headers': 'x-requested-with'
-             },
-             success: function (data) {
-                console.log(data);
-             },
-             error:function(er){
-             }
-         });
 
+        //Ajax get current
          $.ajax({
              method: 'get',
              url: "https://weather-api-cors.herokuapp.com/iot",
@@ -62,17 +41,106 @@ function getCurrentWeather(){
                'Access-Control-Allow-Headers': '*',
              },
              success: function (data){
-               alert('success')
-             },
+                console.log(data)
+                document.getElementById("txtTempreture").innerHTML= Math.round(data.temperature * 100) / 100+ '° C - '+convertCtoF(data.temperature)+'° F'
+                document.getElementById("txtHumidity").innerHTML='Độ ẩm: ' + Math.round(data.humidity * 100) / 100+' %'
+                document.getElementById("txtTime").innerHTML='Cập nhật lúc: ' + timeConverter(data.time)
+               
+               
+               //Chỉnh giờ tiếp
+                getNextPredict(data.temperature,data.humidity)
+
+
+                //Xét độ ẩm
+                //set icon current
+                if(parseFloat(data.humidity)>80){
+                    document.getElementById("imgCurrent").src="../images/rainy.svg";
+                }
+                else if(parseFloat(data.humidity)>40){
+                    document.getElementById("imgCurrent").src="../images/cloudy.svg";
+                }
+                else {
+                    document.getElementById("imgCurrent").src="../images/day.svg";
+                }           
+            },
              error:function(er){
               alert('somemistkae')
              }
-           });
-      
-      
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open( "GET", 'https://weather-api-cors.herokuapp.com/iot', false ); // false for synchronous request
-        xmlHttp.send( null );
-        alert( xmlHttp.responseText);
-        
+           })  
+}
+
+function convertCtoF(degree){
+    degree=parseFloat(degree);
+    var f;
+    f= 1.8*degree+32;
+    return Math.round(f * 100) / 100
+}
+function timeConverter(UNIX_timestamp) {
+    var a = new Date(UNIX_timestamp * 1000);
+    // var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var hour = a.getHours()-7;
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    sec = sec < 10 ? '0'+sec : sec;
+    // var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+    var time = hour + ':' + min + ':' + sec;
+    return time;
+}
+
+
+function getNextPredict(temp,humid){
+   
+    $.ajax({
+        method: 'get',
+        url: 'https://weather-api-cors.herokuapp.com/iot/predict/?temp='+temp +'&hum='+humid,
+        // dataType: 'json',
+        // cache:false,
+        // cors: true ,
+        contentType:'application/json; charset=utf-8',
+        // secure: true,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': 'true',
+          "Access-Control-Allow-Methods": "GET",
+          'Access-Control-Allow-Headers': '*',
+        },
+        success: function (data){
+            // console.log(data)
+           document.getElementById('txtTempreratureNext').innerHTML ='Nhiệt độ: '+ Math.round(data.temperature * 100) / 100+ '° C - '+convertCtoF(data.temperature)+'° F'
+           document.getElementById("txtHumidityNext").innerHTML ='Độ ẩm: ' + Math.round(data.humidity * 100) / 100+' %'
+           
+
+           //XET BACKGROUND
+           if(parseFloat(data.humidity)>80){
+            document.getElementById("imgPredict").src="../images/rainy.svg";
+            }
+            else if(parseFloat(data.humidity)>40){
+                document.getElementById("imgPredict").src="../images/cloudy.svg";
+            }
+            else {
+                document.getElementById("imgPredict").src="../images/day.svg";
+            }    
+        },
+        error:function(er){
+         alert('somemistkae')
+        }
+      })  
+}
+
+function setBackground(){
+    var a = new Date();
+    var hours = a.getHours();
+    var minutes = a.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    // hours = hours % 12;
+    // hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    // var strTime = hours + ':' + minutes + ' ' + ampm;
+    if(hours>6 && hours<=18) {
+        //Sang
+        $('body').css('background-image', 'url(../images/bgday.jpg)');
+
+    }else{
+    }
+
 }
